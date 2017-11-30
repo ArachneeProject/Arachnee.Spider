7c655da31172ed1b69ae303eb2f9016e22b692d6
Unknown <email>
2017-11-30 21:24:30 +0100

    Add ArchiveDownloader class

---
+++ Spider/ArchiveDownloader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using RestSharp;
+
+namespace Spider
+{
+    public class ArchiveDownloader
+    {
+        private readonly RestClient _client = new RestClient("http://files.tmdb.org/p/exports/");
+
+        public void GetMovies(DateTime archiveDate, string destinationFolder)
+        {
+            if (archiveDate.Kind != DateTimeKind.Utc)
+            {
+                throw new ArgumentException(nameof(archiveDate), $"{nameof(archiveDate)} is not UTC.");
+            }
+
+            if (archiveDate > DateTime.UtcNow)
+            {
+                throw new ArgumentException(nameof(archiveDate),
+                    $"{nameof(archiveDate)} is not valid because it is in the future.");
+            }
+
+            if (!Directory.Exists(destinationFolder))
+            {
+                throw new DirectoryNotFoundException($"Folder doesn't exist at \"{destinationFolder}\"");
+            }
+
+            string resource = $"movie_ids_{archiveDate.Month:00}_{archiveDate.Day:00}_{archiveDate.Year:0000}.json.gz";
+            
+            var request = new RestRequest(resource, Method.GET);
+
+            var response = _client.Execute(request);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new FailedRequestException(_client.BuildUri(request).ToString(), response);
+            }
+
+            string filePath = Path.Combine(destinationFolder, resource);
+
+            File.WriteAllBytes(filePath, response.RawBytes);
+        }
+    }
+}
---
+++ Spider/FailedRequestException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RestSharp;
+
+namespace Spider
+{
+    public class FailedRequestException : Exception
+    {
+        public IRestResponse Response { get; }
+
+        public FailedRequestException(string request, IRestResponse response) 
+            : base($"Request \"{request}\" failed with code {(int)response.StatusCode} {response.StatusCode}: {response.ErrorMessage}")
+        {
+            Response = response;
+        }
+    }
+}
